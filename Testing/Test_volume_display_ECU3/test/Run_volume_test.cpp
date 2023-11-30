1913e214003f2963d917fba0ef5ec1684b497385
DaniRuan <93226207+DaniRuan@users.noreply.github.com>
2023-11-30 14:36:15 -0600

    Update Run_volume_test.cpp

--- Testing/Test_volume_display_ECU3/test/Run_volume_test.cpp
+++ Testing/Test_volume_display_ECU3/test/Run_volume_test.cpp
@@ -1,14 +1,14 @@
 /* Script where the module test cases are executed */
 #include <Arduino.h>
 #include <unity.h>
-#include <Set_volume_test.hpp>
+#include <Set_value_test.hpp>
 
 void setup(){
   Serial.begin(9600);
   delay(1000);
 
   UNITY_BEGIN();
-      RUN_TEST(test_volume_variation);
+      RUN_TEST(test_value_variation);
   UNITY_END();
 }
 
